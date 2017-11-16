1596026a0e240a917a09080bd31671bafc99de08
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-11-16 15:44:40 -0700

    Replace generic seeds.db with ours

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+fourteeners_attributes = [ 
+	{ name: 'Grays Peak', elevation: 14027 },
+	{ name: 'Torreys Peak', elevation: 14148 },
+	{ name: 'Mount Sherman', elevation: 500 }
+]
+
+trails_data = [
+  	{
+  		attributes: {name: 'Grays and Torreys trail', distance: 12, elevation_gain: 2000 },
+	    fourteener_names: ['Grays Peak', 'Torreys Peak']
+	},
+
+    {
+    	attributes: { name: 'ol Sherman', distance: 3.87, elevation_gain: 500 },
+    	fourteener_names: ['Mount Sherman']
+    }
+]
+
+fourteeners_count = 0
+fourteeners_attributes.each do |attributes|
+	Fourteener.create!(attributes)
+	fourteeners_count += 1
+end
+puts "created #{fourteeners_count} fourteeners"
+
+
+trails_count = 0
+trails_data.each do |datum|
+	trail = Trail.create!(datum[:attributes])
+
+	trail.fourteeners = datum[:fourteener_names].map{ |name| Fourteener.find_by(name: name) }
+
+	trails_count += 1
+end
+puts "created #{trails_count} trails"
