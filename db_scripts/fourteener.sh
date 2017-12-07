a25cc7b66faf60c4bc4637c56d6e44eb8c62d3a3
lawalton <liwa9544@colorado.edu>
2017-12-06 20:55:39 -0700

    Fixed fourteener db script

--- db_scripts/fourteener.sh
+++ db_scripts/fourteener.sh
@@ -2,6 +2,6 @@
 #Script to create ruby list of fourteeners from CSV file
 awk -F "," '
 BEGIN {print "fourteeners_attributes=["}
-{if (NR!=1) {print "\t{name:\""$2"\", surname:\""$1"\", elevation_in_feet:"$3", rank:"$4", location_lat:"$4", location_long:"$5", forest:\""$6"\", range:\""$7"\", classification:"$8", exposure:"$9"},"} }
+{if (NR!=1) {print "\t{name:\""$2"\", surname:\""$1"\", elevation_in_feet:"$3", rank:"$4", location_lat:"$5", location_long:"$6", forest:\""$7"\", range:\""$8"\", classification:"$9", exposure:"$10"},"} }
 END{print "]"}
 ' $1
