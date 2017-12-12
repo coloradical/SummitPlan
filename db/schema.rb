895e43b654cf70e1abda63295e649634bb5a0c8f
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-12-11 21:36:34 -0700

    Change kml data type to string for URLs. Closes #22

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171210095615) do
+ActiveRecord::Schema.define(version: 20171212033939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20171210095615) do
     t.decimal  "four_x_four_required"
     t.string   "additional_info_url"
     t.text     "description"
-    t.binary   "kml"
+    t.string   "kml"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
